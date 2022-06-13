1718ac2aa69a64713794f98b261c0cacca15fd16
Xiaohu Zheng <zxh635148320@gmail.com>
2022-06-13 19:53:18 +0000

    Fix indentation errors

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -6,27 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+/**
+ * Handles requests sent to the /hello URL. Try running a server and navigating
+ * to /hello!
+ */
 @WebServlet("/message")
 public class HelloWorldServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String[] messages = {"I am interested in Unreal Engine 5!", 
-													"I like C++, but I am using java now!",
-													"I often post leetcode solutions!"};
-		
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String[] messages = { "I am interested in Unreal Engine 5!",
+				"I like C++, but I am using java now!",
+				"I often post leetcode solutions!" };
+
 		response.setContentType("application/json;");
-    response.getWriter().println("{ \"messages\" : "+converListToJson(messages) +" }");
-  }
+		response.getWriter().println("{ \"messages\" : " + converListToJson(messages) + " }");
+	}
 
 	private String converListToJson(String[] list) {
 		String str = "[";
-		int n = list.length;
-		for(int i = 0; i < n-1; ++i) {
+		int last = list.length - 1;
+		for (int i = 0; i < last; ++i) {
 			str += "\"" + list[i] + "\",";
 		}
-		str += "\"" + list[n-1] + "\"]";
+		str += "\"" + list[last] + "\"]";
 		return str;
 	}
 }
